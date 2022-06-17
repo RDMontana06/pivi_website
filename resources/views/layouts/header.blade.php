6ae0c2ed8bc97ca9c09726bdf6a85caf64c98f7e
RDMontana06 <montanareymart06@gmail.com>
2022-06-17 12:39:49 +0800

    Refactor logo code

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -53,11 +53,12 @@
 	<header id="header" class="d-flex align-items-center">
 		<div class="container d-flex align-items-center justify-content-between">
 
-			<h1 class="logo"><a href="{{ url('/') }}">
+			{{-- <h1 class="logo"><a href="{{ url('/') }}">
 					<img src="{{ asset('img/PIVI_FINAL_PNG-02.png') }}">
-				</a></h1>
+				</a></h1> --}}
 			<!-- Uncomment below if you prefer to use an 	mage logo -->
-			<!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
+			<a href="{{ url('/') }}" class="logo"><img src="{{ asset('img/PIVI_FINAL_PNG-02.png') }}"
+					alt=""></a>
 
 			<nav id="navbar" class="navbar">
 				<ul>
